d897146f3d73a21a3e3169897bac374d7602a3d9
sholeh007 <sholeh09@outlook.com>
2021-08-19 14:07:29 +0900

    setting database

--- app/config.js
+++ app/config.js
@@ -2,4 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   serviceName: process.env.SERVICE_NAME,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbName: process.env.DB_NAME,
 };
---
+++ database/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { dbHost, dbName, dbPort } = require("../app/config");
+
+mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+module.exports = db;

807de134717a4222373d93ece942cfe9c94663ee
sholeh007 <sholeh09@outlook.com>
2021-08-20 14:33:24 +0900

    create schema & model product

---
+++ app/product/model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const { model, Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: [3, "Panjang nama makanan minimal 3 karakter"],
+      require: [true, "Nama product harus diisi"],
+    },
+    description: {
+      type: String,
+      maxlength: [1000, "Panjang deskripsi maksimal 1000 karakter"],
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    image_url: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("product", productSchema);

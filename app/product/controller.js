f0ef157d94f1bd474d7fa199a2e80dfe15e1bb1c
sholeh007 <sholeh09@outlook.com>
2021-08-21 09:28:02 +0900

    add error handler product & change view engine

--- app/product/controller.js
+++ app/product/controller.js
@@ -1,11 +1,22 @@
 const Product = require("./model");
 
 async function store(req, res, next) {
-  const payload = req.body;
-  const product = new Product(payload);
+  try {
+    const payload = req.body;
+    const product = new Product(payload);
 
-  await product.save();
-  return res.status(200).json(product);
+    await product.save();
+    return res.status(200).json(product);
+  } catch (err) {
+    if (err & (err.name === "ValidationError")) {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
+    next(err);
+  }
 }
 
 module.exports = {

0befeb99fdf29d79b88524557f299c16f10eb4fe
sholeh007 <sholeh09@outlook.com>
2021-08-24 11:03:57 +0900

    adding upload image on controller product

--- app/config.js
+++ app/config.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const path = require("path");
 
 module.exports = {
+  rootPath: path.resolve(__dirname, ".."),
   serviceName: process.env.SERVICE_NAME,
   dbHost: process.env.DB_HOST,
   dbPort: process.env.DB_PORT,
--- app/product/controller.js
+++ app/product/controller.js
@@ -1,12 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+const config = require("../config");
 const Product = require("./model");
 
 async function store(req, res, next) {
   try {
     const payload = req.body;
-    const product = new Product(payload);
 
-    await product.save();
-    return res.status(200).json(product);
+    if (req.file) {
+      const tmp_path = req.file.path;
+      const originalExt =
+        req.file.originalname.split(".")[
+          req.file.originalname.split(".").length - 1
+        ];
+      const fileName = req.file.filename + "." + originalExt;
+      const target_path = path.resolve(
+        config.rootPath,
+        `public/images/${fileName}`
+      );
+      const src = fs.createReadStream(tmp_path);
+      const dest = fs.createWriteStream(target_path);
+      src.pipe(dest);
+
+      src.on("end", async () => {
+        try {
+          const product = new Product({ ...payload, image_url: fileName });
+          await product.save();
+          return res.status(200).json(product);
+        } catch (err) {
+          fs.unlinkSync(target_path);
+          if (err.name === "ValidationError") {
+            return res.json({
+              error: 1,
+              message: err.message,
+              fields: err.errors,
+            });
+          }
+          next(err);
+        }
+      });
+      src.on("error", async () => {
+        next(err);
+      });
+    } else {
+      const product = new Product(payload);
+      await product.save();
+      return res.status(200).json(product);
+    }
   } catch (err) {
     if (err.name === "ValidationError") {
       return res.json({

e963adeeac30c882f530c976645582de8069e5b1
sholeh007 <sholeh09@outlook.com>
2021-08-24 11:22:41 +0900

    edit route post products

--- app/product/route.js
+++ app/product/route.js
@@ -1,7 +1,12 @@
+const os = require("os");
 const router = require("express").Router();
 const multer = require("multer");
 const productController = require("./controller");
 
-router.post("/products", multer().none(), productController.store);
+router.post(
+  "/products",
+  multer({ dest: os.tmpdir() }).single("image"),
+  productController.store
+);
 
 module.exports = router;

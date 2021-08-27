5a0c370c2101591d5dad6abd6e330eb1b5533d31
sholeh007 <sholeh09@outlook.com>
2021-08-27 19:04:46 +0900

    create model tag

---
+++ app/tag/model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const tagSchema = new Schema({
+  name: {
+    type: String,
+    minlength: [3, "Panjang tag min 3 karakter"],
+    maxlength: [20, "Panjang tag max 20 karakter "],
+    required: [true, "tag harus diisi"],
+  },
+});
+
+module.exports = model("tag", tagSchema);

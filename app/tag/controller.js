98344779c37692aad2b2b28109b493de19aca012
sholeh007 <sholeh09@outlook.com>
2021-08-28 08:44:18 +0900

    added endpoint delete and update tag

--- app/tag/controller.js
+++ app/tag/controller.js
@@ -20,6 +20,39 @@ async function store(req, res, next) {
   }
 }
 
+async function update(req, res, next) {
+  try {
+    const payload = req.body;
+    const tag = await Tag.findByIdAndUpdate(req.params.id, payload, {
+      new: true,
+      runValidators: true,
+    });
+
+    return res.status(200).json(tag);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors,
+      });
+    }
+    next(err);
+  }
+}
+
+async function destroy(req, res, next) {
+  try {
+    const tag = await Tag.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json(tag);
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   store,
+  update,
+  destroy,
 };
--- app/tag/router.js
+++ app/tag/router.js
@@ -3,5 +3,7 @@ const multer = require("multer");
 const tagController = require("./controller");
 
 router.post("/tags", multer().none(), tagController.store);
+router.put("/tags/:id", multer().none(), tagController.update);
+router.delete("/tags/:id", tagController.destroy);
 
 module.exports = router;

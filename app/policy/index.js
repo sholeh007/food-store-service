fd915dfea60676d64d4ecafcef73c3b886b1a683
sholeh007 <sholeh09@outlook.com>
2021-09-11 10:30:28 +0900

    configurasi otorisasi

---
+++ app/policy/index.js
@@ -0,0 +1,52 @@
+const { AbilityBuilder, Ability } = require("@casl/ability");
+
+const policies = {
+  guest(user, { can }) {
+    can("read", "Product");
+  },
+  user(user, { can }) {
+    // melihat daftar 'Order'
+    can("view", "Order");
+    // membuat order
+    can("create", "Order");
+    // membaca 'Order' miliknya
+    can("read", "Order", { user_id: user._id });
+    // mengupdate data dirinya
+    can("update", "User", { _id: user._id });
+    // membaca 'Cart' miliknya
+    can("read", "Cart", { user_id: user.id });
+    // mengupdate 'Cart' miliknya
+    can("update", "Cart", { user_id: user._id });
+    // melihat daftar 'DeliveryAddress'
+    can("view", "DeliveryAddress");
+    // membuat `DeliveryAddress`
+    can("create", "DeliveryAddress", { user_id: user._id });
+    // membaca `DeliveryAddress` miliknya
+    can("read", "DeliveryAddress", { user_id: user._id });
+    // mengupdate `DeliveryAddress` miliknya
+    can("update", "DeliveryAddress", { user_id: user._id });
+    // menghapus `DeliveryAddress` miliknya
+    can("delete", "DeliveryAddress", { user_id: user._id });
+    // membaca `Invoice` miliknya
+    can("read", "Invoice", { user_id: user._id });
+  },
+  admin(user, { can }) {
+    can("manage", "all");
+  },
+};
+
+function policyFor(user) {
+  const builder = new AbilityBuilder();
+
+  if (user && typeof policies[user.role] === "function") {
+    policies[user.role](user, builder);
+  } else {
+    policies.guest(user, builder);
+  }
+
+  return new Ability(builder.rules);
+}
+
+module.exports = {
+  policyFor,
+};

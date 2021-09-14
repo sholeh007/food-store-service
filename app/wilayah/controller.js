28c0c3deda1cb19fbfa8f68836e2387f19a9c2c1
sholeh007 <sholeh09@outlook.com>
2021-09-14 14:00:27 +0900

    creating endpoint kabupaten

--- app/wilayah/controller.js
+++ app/wilayah/controller.js
@@ -15,6 +15,27 @@ async function getProvinsi(req, res, next) {
   }
 }
 
+async function getKabupaten(req, res, next) {
+  const db_kabupaten = path.resolve(__dirname, "./data/regencies.csv");
+  try {
+    const { kode_induk } = req.query;
+    const data = await csv().fromFile(db_kabupaten);
+
+    if (!kode_induk) return res.json(data);
+
+    const kabupaten = data.filter(
+      (kabupaten) => kabupaten.kode_provinsi === kode_induk
+    );
+    return res.status(200).json(kabupaten);
+  } catch (err) {
+    return res.json({
+      error: 1,
+      message: "Tidak bisa mengambil data kabupaten",
+    });
+  }
+}
+
 module.exports = {
   getProvinsi,
+  getKabupaten,
 };
--- app/wilayah/router.js
+++ app/wilayah/router.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("./controller");
 
 router.get("/wilayah/provinsi", controller.getProvinsi);
+router.get("/wilayah/kabupaten", controller.getKabupaten);
 
 module.exports = router;

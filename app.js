02aeff863081e9493c73bf755abd6ea43dd5b6fc
sholeh007 <sholeh09@outlook.com>
2021-09-27 18:43:29 +0900

    adding cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 const { decodeToken } = require("./app/auth/middlewware");
 const productRouter = require("./app/product/router");
 const categoryRouter = require("./app/category/router");
@@ -25,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors());
 app.use(decodeToken());
 
 app.use("/api", productRouter);

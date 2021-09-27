162a6eeb5a9d2adfd4f5026dd3646aa779c9b428
sholeh007 <sholeh09@outlook.com>
2021-09-27 15:58:33 +0900

    created mongoose hook invoice on order model

--- app/order/model.js
+++ app/order/model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const AutoIncrement = require("mongoose-sequence")(mongoose);
+const Invoice = require("../invoice/model");
 
 const { Schema, model } = mongoose;
 
@@ -42,6 +43,25 @@ virtual.get(function () {
   }, 0);
 
   return totalItem;
+});
+
+// mongoose hook
+orderSchema.post("save", async function () {
+  const sub_total = this.order_items.reduce((sum, item) => {
+    return (sum += item.price * item.qty);
+  }, 0);
+
+  // create new invoice object
+  const invoice = new Invoice({
+    user: this.user,
+    order: this._id,
+    sub_total,
+    delivery_fee: parseInt(this.delivery_fee),
+    total: parseInt(sub_total + this.delivery_fee),
+    delivery_address: this.delivery_address,
+  });
+
+  await invoice.save();
 });
 
 module.exports = model("Order", orderSchema);

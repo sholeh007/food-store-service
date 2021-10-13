c0a462ec2c173b8a758051974ca0a6447ac9ee62
sholeh007 <sholeh09@outlook.com>
2021-10-13 15:12:34 +0900

    adding config cors credentials & origin

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
 app.use(decodeToken());
 
 app.use("/api", productRouter);
